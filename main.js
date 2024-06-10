3d9ef7d043fab1bd33ae0bfc29fde9b09ae17b03
AR-6899 <animeshrath621@gmail.com>
2024-06-10 22:54:52 +0530

    .env added

--- main.js
+++ main.js
@@ -1,35 +1,36 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const todo_route = require('./routes/todo.js');
-const Music = require('./models/music.model.js');
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const todo_route = require("./routes/todo.js");
+const Music = require("./models/music.model.js");
 
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT || 5000;
 
-mongoose.connect('mongodb://127.0.0.1/study')
-    .then(() => {
-        console.log("DB connected");
-    })
-    .catch(() => {
-        console.log("DB connection failed");
+mongoose
+  .connect(`${process.env.MONGODB_URL}`)
+  .then(() => {
+    console.log("DB connected successfully !!");
+    app.listen(PORT, () => {
+      console.log(`Server running at http://localhost:${PORT}`);
     });
+  })
+  .catch(() => {
+    console.log("DB connection failed !!");
+  });
 
-app.use('/public', express.static('public'));
-app.use('/assets', express.static('assets'));
+app.use("/public", express.static("public"));
+app.use("/assets", express.static("assets"));
 app.use(express.json());
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
-app.get('/', (req, res) => {
-    res.render('index');
+app.get("/", (req, res) => {
+  res.render("index");
 });
 
-app.get('/music/:album', async (req, res) => {
-    let data = await Music.find({ Album: `${req.params.album}` }, { _id: 0 });
-    res.status(200).json(data);
+app.get("/music/:album", async (req, res) => {
+  let data = await Music.find({ Album: `${req.params.album}` }, { _id: 0 });
+  res.status(200).json(data);
 });
 
-app.use('/todo', todo_route);
-
-app.listen(port, () => {
-    console.log(`Server running at ${port}`);
-});
+app.use("/todo", todo_route);
